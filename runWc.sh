70c867d69cf9ae5328397c821edf9e7305a2c62d
dragos <dragos.balan@adswizz.com>
2024-09-23 13:29:40 +0300

    improved run.sh

--- runWc.sh
+++ runWc.sh
@@ -3,4 +3,4 @@
  spark-submit --master local --class thoughtworks.wordcount.WordCount \
     target/scala-2.12/tw-pipeline_2.12-0.1.0-SNAPSHOT.jar \
     "./src/main/resources/data/words.txt" \
-    ./output 2 >&1 | tee wc.log
+    ./output 2>&1 | tee wc.log
